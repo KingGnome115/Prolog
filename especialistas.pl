2c88b112be6cdaa01ef1055b0237cd8816a91f6d
King Gnomo <kevinjec115@gmail.com>
2021-08-16 13:50:06 -0500

    segundo archivo de prolog

---
+++ especialistas.pl
@@ -0,0 +1,8 @@
+especialista(ana,'redes neuronales').
+especialista(ana,'aprendizaje por refuerzo').
+especialistas(juan,'redes neuronales').
+especialistas(pepe,'algoritmos genericos').
+especialistas(sonia,'busqueda en grafos').
+especialistas(juan,'logica de primer orden').
+especialista(rodrigo,'aprendizaje supervisado').
+/*suma(A,B,C):-C is A*B.*/
